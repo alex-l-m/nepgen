04dc65db6ac5d824ead2b79c9cbea6748ca8b4fa
zhuan08 <zhuang1356@gmail.com>
2024-03-13 12:50:07 -0400

    Updated top_combinations bot_combinations
    To just one function: sub_one_group

--- combinations.py
+++ combinations.py
@@ -16,6 +16,14 @@ ligand_top = Chem.MolFromSmiles('SC1=CN=C(C2=N1)N3C(N2C)[Ir]4C5N(C)C6=NC(S)=CN=C
 ligand_bot = Chem.MolFromSmiles('SC1=CN=C2C(N3C(N2C)[Ir]4C5N(C)C6=NC=C(N=C6N5C7=CC(C(F)(F)F)=CC3=C74)S)=N1')
 
 
+def smiles_to_rdkit_mol(sub_list):  # converts list of smiles strings to rdkit molecules
+    rdkit_list = []                 # list of rdkit molecules
+    for sub in sub_list:            # goes through lists and does conversion
+        rdkit_mol = Chem.MolFromSmiles(sub)
+        rdkit_list.append(rdkit_mol)
+    return rdkit_list
+
+
 def sub_combinations(ligand, sub1, sub2, sub3,              # returns a set of all combinations of substitutions
                      p1=Chem.MolFromSmiles('S'),            # limitation, 3 lists, 3 groups to substitute
                      p2=Chem.MolFromSmiles('Cl'),
@@ -32,31 +40,26 @@ def sub_combinations(ligand, sub1, sub2, sub3,              # returns a set of a
     return substituted_ligands
 
 
-set1_unfiltered = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
-set1 = set(smiles for smiles in set1_unfiltered if Chem.MolFromSmiles(smiles) is not None)
-Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set1], molsPerRow=8, subImgSize=(200, 200)).show()
+# set1_unfiltered = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
+# set1 = set(smiles for smiles in set1_unfiltered if Chem.MolFromSmiles(smiles) is not None)
+# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set1], molsPerRow=8, subImgSize=(200, 200)).show()
 
 
-def top_combinations(ligand, top_sub, p1=Chem.MolFromSmiles('S')):
-    top_ligands = set()
-    for x in top_sub:
-        ligand_w_top_sub = AllChem.ReplaceSubstructs(ligand, p1, Chem.MolFromSmiles(x), True)[0]
-        if ligand_w_top_sub is not None:
-            top_ligands.add(Chem.MolToSmiles(ligand_w_top_sub, True))
-    return top_ligands
-
-
-def bot_combinations(ligand, bot_sub, p1=Chem.MolFromSmiles('S')):
-    bot_ligands = set()
-    for x in bot_sub:
-        ligand_w_bot_sub = AllChem.ReplaceSubstructs(ligand, p1, Chem.MolFromSmiles(x), True)[0]
-        if ligand_w_bot_sub is not None:
-            bot_ligands.add(Chem.MolToSmiles(ligand_w_bot_sub, True))
-    return bot_ligands
-
+def sub_one_group(ligand, sub, p1=Chem.MolFromSmiles('S')):
+    substituted_ligands = set()
+    for x in sub:
+        ligand_w_sub = AllChem.ReplaceSubstructs(ligand, p1, Chem.MolFromSmiles(x), True)[0]
+        if ligand_w_sub is not None:
+            substituted_ligands.add(Chem.MolToSmiles(ligand_w_sub, True))
+    return substituted_ligands
 
 # # Save SMILES strings of substituted ligands
 # output_smiles_filename = 'substituted_ligands.txt'
 # with open(output_smiles_filename, 'w') as f:
 #     for x in set1:
 #         f.write(x + '\n')
+
+
+set1_unfiltered = sub_one_group(ligand_top, sub_1)
+set1 = set(smiles for smiles in set1_unfiltered if Chem.MolFromSmiles(smiles) is not None)
+Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set1], molsPerRow=8, subImgSize=(200, 200)).show()
