d3493664eb122a3c69bdf61761d42d13080b83d2
zhuan08 <zhuang1356@gmail.com>
2024-03-25 23:06:21 -0400

    Fixed ligand_top and ligand_bot with correct placeholder atoms

--- combinations.py
+++ combinations.py
@@ -60,20 +60,20 @@ def sub_one_group(ligand, sub, p1=Chem.MolFromSmiles('S')):
 #         f.write(x + '\n')
 
 # Show images of sub_combinations
-# all combinations for ligand_l
-set_l = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
-set_fl = set(smiles for smiles in set_l if Chem.MolFromSmiles(smiles) is not None)
-Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fl], molsPerRow=8, subImgSize=(200, 200)).show()
+# # all combinations for ligand_l
+# set_l = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
+# set_fl = set(smiles for smiles in set_l if Chem.MolFromSmiles(smiles) is not None)
+# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fl], molsPerRow=8, subImgSize=(200, 200)).show()
 
 # all combinations for ligand_top
-set_t = sub_combinations(ligand_top, sub_1, sub_2, sub_3)
-set_ft = set(smiles for smiles in set_t if Chem.MolFromSmiles(smiles) is not None)
-Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_ft], molsPerRow=8, subImgSize=(200, 200)).show()
+# set_t = sub_combinations(ligand_top, sub_1, sub_2, sub_3)
+# set_ft = set(smiles for smiles in set_t if Chem.MolFromSmiles(smiles) is not None)
+# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_ft], molsPerRow=8, subImgSize=(200, 200)).show()
 
 # all combinations for ligand_bot
-set_b = sub_combinations(ligand_bot, sub_1, sub_2, sub_3)
-set_fb = set(smiles for smiles in set_b if Chem.MolFromSmiles(smiles) is not None)
-Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fb], molsPerRow=8, subImgSize=(200, 200)).show()
+# set_b = sub_combinations(ligand_bot, sub_1, sub_2, sub_3)
+# set_fb = set(smiles for smiles in set_b if Chem.MolFromSmiles(smiles) is not None)
+# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fb], molsPerRow=8, subImgSize=(200, 200)).show()
 
 # Show images of sub_one_group
 # sub_one_set = sub_one_group(ligand_bot, sub_1)
