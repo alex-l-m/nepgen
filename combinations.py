d252d5929010b138bb04f0a09e6ae0c39ee01eb2
zhuan08 <zhuang1356@gmail.com>
2024-03-25 22:35:56 -0400

    Added source column

--- combinations.py
+++ combinations.py
@@ -12,8 +12,12 @@ sub_2 = ["[CH3]", "[H]"]
 sub_3 = substitution_smiles
 
 ligand_l = Chem.MolFromSmiles('SC1=C(N=C(C2=N1)N3C(N2C)=[Ir]4=C5N(C)C6=NC(S)=C(N=C6N5C7=C(Cl)C(I)=C(Cl)C3=C74)S)S')
-ligand_top = Chem.MolFromSmiles('SC1=CN=C(N2C(N3C)=[Ir]4=C5N(C6=NC(S)=CN=C6N5C7=CC(C(F)(F)F)=CC2=C74)C)C3=N1')
-ligand_bot = Chem.MolFromSmiles('SC1=CN=C2C(N3C(N2C)=[Ir]4=C5N(C6=NC=C(S)N=C6N5C7=CC(C(F)(F)F)=CC3=C74)C)=N1')
+ligand_top = Chem.MolFromSmiles('SC1=CN=C(C2=N1)N3C(N2C)=[Ir]4=C5N(C)C6=NC(S)=CN=C6N5C7=C(Cl)C(I)=C(Cl)C3=C74')
+ligand_bot = Chem.MolFromSmiles('CN1C(N2C3=NC(S)=CN=C31)=[Ir]4=C5N(C)C6=NC=C(S)N=C6N5C7=C(Cl)C(I)=C(Cl)C2=C74')
+
+Draw.MolToImage(Chem.AddHs(ligand_l)).show()
+Draw.MolToImage(Chem.AddHs(ligand_top)).show()
+Draw.MolToImage(Chem.AddHs(ligand_bot)).show()
 
 
 def smiles_to_rdkit_mol(sub_list):  # converts list of smiles strings to rdkit molecules
@@ -56,11 +60,22 @@ def sub_one_group(ligand, sub, p1=Chem.MolFromSmiles('S')):
 #         f.write(x + '\n')
 
 # Show images of sub_combinations
-# set1_unfiltered = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
-# set1 = set(smiles for smiles in set1_unfiltered if Chem.MolFromSmiles(smiles) is not None)
-# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set1], molsPerRow=8, subImgSize=(200, 200)).show()
+# all combinations for ligand_l
+set_l = sub_combinations(ligand_l, sub_1, sub_2, sub_3)
+set_fl = set(smiles for smiles in set_l if Chem.MolFromSmiles(smiles) is not None)
+Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fl], molsPerRow=8, subImgSize=(200, 200)).show()
+
+# all combinations for ligand_top
+set_t = sub_combinations(ligand_top, sub_1, sub_2, sub_3)
+set_ft = set(smiles for smiles in set_t if Chem.MolFromSmiles(smiles) is not None)
+Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_ft], molsPerRow=8, subImgSize=(200, 200)).show()
+
+# all combinations for ligand_bot
+set_b = sub_combinations(ligand_bot, sub_1, sub_2, sub_3)
+set_fb = set(smiles for smiles in set_b if Chem.MolFromSmiles(smiles) is not None)
+Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in set_fb], molsPerRow=8, subImgSize=(200, 200)).show()
 
 # Show images of sub_one_group
-sub_one_set = sub_one_group(ligand_bot, sub_1)
-sos_set = set(smiles for smiles in sub_one_set if Chem.MolFromSmiles(smiles) is not None)
-Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in sos_set], molsPerRow=5, subImgSize=(200, 200)).show()
+# sub_one_set = sub_one_group(ligand_bot, sub_1)
+# sos_set = set(smiles for smiles in sub_one_set if Chem.MolFromSmiles(smiles) is not None)
+# Draw.MolsToGridImage([Chem.AddHs(Chem.MolFromSmiles(x)) for x in sos_set], molsPerRow=5, subImgSize=(200, 200)).show()
